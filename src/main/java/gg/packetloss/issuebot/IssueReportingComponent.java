d49d1657cdf92e78c9be29bc5fb9dd4f9c19b274
Wyatt Childers <wchilders@nearce.com>
2020-01-24 19:38:26 -0500

    Release as GPL

--- src/main/java/gg/packetloss/issuebot/IssueReportingComponent.java
+++ src/main/java/gg/packetloss/issuebot/IssueReportingComponent.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Minecraft Github Issue Bot.
+ *
+ * Minecraft Github Issue Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Minecraft Github Issue Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Minecraft Github Issue Bot.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package gg.packetloss.issuebot;
 
 import com.google.gson.Gson;
